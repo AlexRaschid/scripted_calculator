4160248b2ea0323d864b1d9ee28ce0b28fc3bf15
Alex R <alexraschid1337@gmail.com>
2016-08-04 10:31:00 -0400

    Changed guts.js to RubberDucky Code the Functions

---
+++ js/guts.js
@@ -0,0 +1,50 @@
+// Takes X and divides it by Y
+function divide(x,y)
+{
+  return x/y;
+}
+
+// Takes X and multiplies it by Y
+
+function multiply(x,y){
+    return x*y;
+}
+
+// Takes X and subtracts Y from it
+
+function subtract(x,y){
+    return x-y;
+}
+
+//  Takes X and adds Y to it
+function add(x,y){
+    return x+y;
+    
+}
+
+// Takes X and raises it to the power of Y
+
+function power(x,y){
+    return Math.pow(x,y)
+}
+
+// Takes X and put it in square root form
+
+function square_root(x){
+    return Math.sqrt(x);
+}
+
+// The common logarithm of x is the power to which the number 10 must be raised to obtain the value x.
+// Function below takes X and puts it in a logarithum that out puts what can be made into 10
+
+function logBase10(x){
+    return Math.log(x);
+}
+
+// Function Generates a random number between 1 and 0
+
+function generateRandomNumber(){
+     return Math.random();
+}
+
+// You must implement the rest of the functions.
